218c24a63e522aa76ad285146cc937cb38ad3138
oscar leon <roleonm@gmail.com>
2017-09-05 14:21:45 -0400

    ancho de imagen en el navbar y background de actividades + libreria taggd en imagen de direcotrio

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,91 @@
-var transparentDemo = true;
-var fixedTop = false;
+$(document).ready(function(){
 
-$(window).scroll(function(e) {
-    oVal = ($(window).scrollTop() / 170);
-    $(".blur").css("opacity", oVal);
-    
+	var transparentDemo = true;
+	var fixedTop = false;
+
+	$(window).scroll(function(e) {
+	    oVal = ($(window).scrollTop() / 170);
+	    $(".blur").css("opacity", oVal);
+	    
+	});
+
+
+	var image = document.getElementById('img-directory');
+
+	var options = {};
+	var data = [
+	  Taggd.Tag.createFromObject({
+    "position": {
+      "x": 0.23535,
+      "y": 0.4137
+    },
+    "text": "Vanessa Cuellar"
+  }),
+  
+  Taggd.Tag.createFromObject({
+    "position": {
+      "x": 0.34277,
+      "y": 0.42533
+    },
+    "text": "Zoraida Vaca"
+  }),
+  
+  Taggd.Tag.createFromObject({
+    "position": {
+      "x": 0.50293,
+      "y": 0.41079
+    },
+    "text": "Kendal Plaza"
+  }),
+  
+  Taggd.Tag.createFromObject({
+    "position": {
+      "x": 0.63184,
+      "y": 0.42823
+    },
+    "text": "Carla Muñoz"
+  }),
+  
+  Taggd.Tag.createFromObject({
+    "position": {
+      "x": 0.77051,
+      "y": 0.43696
+    },
+    "text": "Daybela Condori"
+  }),
+  
+  Taggd.Tag.createFromObject({
+    "position": {
+      "x": 0.27441,
+      "y": 0.24219
+    },
+    "text": "Miguel Torrico"
+  }),
+  
+  Taggd.Tag.createFromObject({
+    "position": {
+      "x": 0.41895,
+      "y": 0.23347
+    },
+    "text": "Alfredo Llanos"
+  }),
+  
+  Taggd.Tag.createFromObject({
+    "position": {
+      "x": 0.55176,
+      "y": 0.22765
+    },
+    "text": "Hugo Lopez"
+  }),
+  
+  Taggd.Tag.createFromObject({
+    "position": {
+      "x": 0.68652,
+      "y": 0.27998
+    },
+    "text": "Jose Polar"
+  })
+	];
+
+	var taggd = new Taggd(image, options, data);
 });
-
---
+++ assets/js/taggd-master/docs/options.md
@@ -0,0 +1,18 @@
+# Options
+
+The `options` parameter is an object of options.
+
+Available options are:
+
+```js
+{
+  // The event to show the tag
+  show: 'mouseenter',
+  // The event to hide the tag
+  hide: 'mouseleave',
+
+  // The duration before the tag popup is actually hidden.
+  // If there is spacing between the tag button and popup, and you use mouseover/mouseout to toggle visiblity, you probably want to keep this.
+  hideDelay: 500,
+}
+```
